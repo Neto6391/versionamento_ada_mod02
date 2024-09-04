a159d7d5f95daab7cbd1b5792507c76077cabf9b
Neto6391 <neto6391@gmail.com>
2024-09-04 15:22:05 -0300

    feat: create model user

---
+++ app/db/models/user.py
@@ -0,0 +1,13 @@
+
+
+from sqlalchemy import Column, String, Boolean
+from app.db.base_class import Base
+import uuid
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
