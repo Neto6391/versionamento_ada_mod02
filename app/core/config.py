0f3fe684d135cb4ec99149d1af101613e8ffa79a
Neto6391 <neto6391@gmail.com>
2024-09-04 20:58:04 -0300

    feat: add new env vars in settings

--- app/core/config.py
+++ app/core/config.py
@@ -6,9 +6,10 @@ load_dotenv(dotenv_path='.env')
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "PetPlan"
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     DATABASE_URL: str = os.getenv("DATABASE_URL")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
 
 settings = Settings()
