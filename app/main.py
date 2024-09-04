4b91e6138f41617190b80d3a4b26f45cc2ac86fe
Neto6391 <neto6391@gmail.com>
2024-09-04 15:24:38 -0300

    feat: add endpoint to main and some refactors in main to create env custom ports

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from app.api.v1.endpoints import users
+from app.db.session import SessionLocal, engine
+from app.db.base_class import Base
+from dotenv import load_dotenv
 
-app = FastAPI()
+load_dotenv(dotenv_path='.env')
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI(
+    title="PetPlan API",
+     description="API PetPlan StartUp",
+    version="1.0.0",
+    contact={
+        "name": "Neto6391",
+        "url": "http://example.com",
+        "email": "mail@exemple.com",
+    },
+)
+app.include_router(users.router, prefix="/users", tags=["users"])
+
+
+if __name__ == "__main__":
+    host = os.getenv("HOST", "127.0.0.1")
+    port = int(os.getenv("PORT"))
+    uvicorn.run(app, host=host, port=port, reload=True)
