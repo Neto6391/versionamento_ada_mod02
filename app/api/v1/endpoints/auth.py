27c9dec380da4c2544dd3a1aeb057ebd4e348066
Neto6391 <neto6391@gmail.com>
2024-09-05 01:14:05 -0300

    feat: create logout endpoint and test integration

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from app.db.models.user import User
+from app.schemas.generic import MessageResponse
 from app.services.user_service import get_user_by_email
 from app.schemas.auth import LoginForm, Token
-
-from app.core.security import verify_password, create_access_token
+from app.core.security import verify_password
+from app.services.auth_service import create_access_token, oauth2_scheme, verify_token
+from app.services.logout_service import blacklist_token
 from app.db.session import get_db
 
 router = APIRouter()
@@ -21,3 +24,10 @@ def login_for_access_token(
         )
     access_token = create_access_token(data={"sub": user.email, "scopes": []})
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/logout", response_model=MessageResponse)
+def logout(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db), _: User = Depends(verify_token)):
+    blacklist_token(db=db, token=token)
+    return {"message": "Successfully logged out"}
+
