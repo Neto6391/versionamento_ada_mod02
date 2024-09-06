b971cd313e3fdd3aa43029c41de4defbfcdc74d4
Neto6391 <neto6391@gmail.com>
2024-09-06 12:49:26 -0300

    feat: add feature to include permissions for user by token and register permission group when new user has registered

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,15 +1,28 @@
+from typing import List
+from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from app.db.models.group import Group
 from app.schemas.user import UserCreate
 from app.db.models.user import User
 from app.core.security import get_password_hash
+from app.services.auth_service import verify_token
 
-def create_user(db: Session, user_in: UserCreate):
+def create_user(db: Session, user_in: UserCreate, is_admin: bool):
     existing_user = get_user_by_email(db, user_in.email)
     if existing_user:
         return None
 
     hashed_password = get_password_hash(user_in.password)
     db_user = User(email=user_in.email, hashed_password=hashed_password)
+
+    group_name = "client"
+    if is_admin:
+        group_name = "admin"
+
+    group_client = db.query(Group).filter(Group.name == group_name).first()
+    if group_client:
+        db_user.groups.append(group_client)
+
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -17,3 +30,33 @@ def create_user(db: Session, user_in: UserCreate):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
+
+
+def check_permission(required_role: str):
+    def wrapper(user: User = Depends(verify_token)):
+
+        if len(user.groups) > 0:
+            for group in user.groups:
+                for role in group.roles:
+                    if role.name == required_role:
+                        return True
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions"
+        )
+    return wrapper
+
+def get_user_permissions(user: User) -> List[str]:
+    permissions = set()
+    for group in user.groups:
+        for role in group.roles:
+            for policy in role.policies:
+                if policy.can_create:
+                    permissions.add(f"{role.name.split('_')[0]}_create")
+                if policy.can_read:
+                    permissions.add(f"{role.name.split('_')[0]}_read")
+                if policy.can_update:
+                    permissions.add(f"{role.name.split('_')[0]}_update")
+                if policy.can_delete:
+                    permissions.add(f"{role.name.split('_')[0]}_delete")
+    return list(permissions)
