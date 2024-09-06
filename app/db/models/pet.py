acdfbc54956bcfe942059d42837d4c19869936a2
Neto6391 <neto6391@gmail.com>
2024-09-06 14:44:14 -0300

    feat: create pet model

---
+++ app/db/models/pet.py
@@ -0,0 +1,17 @@
+from uuid import uuid4
+from sqlalchemy import Column, String, ForeignKey, Float, INTEGER
+from sqlalchemy.orm import relationship
+from app.db.base_class import Base
+
+
+class Pet(Base):
+    __tablename__ = "pets"
+
+    id = Column(String, primary_key=True, default=lambda: str(uuid4()))
+    name = Column(String, index=True, nullable=False)
+    age = Column(INTEGER, index=True, nullable=False)
+    weight = Column(Float, index=True, nullable=False)
+    species = Column(String, nullable=False)
+    owner_id = Column(String, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="pets")
