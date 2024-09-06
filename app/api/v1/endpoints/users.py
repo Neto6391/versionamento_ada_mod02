b06a7855fec007545b572e7f58ca25adeca74772
Neto6391 <neto6391@gmail.com>
2024-09-06 12:48:13 -0300

    refactor: endpoint users and include new route to register admin user

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -6,9 +6,20 @@ from app.db.session import get_db
 
 router = APIRouter()
 
-@router.post("/", response_model=UserResponse, summary="Register user", response_description="Service to register user in sign in")
-def register_user(user_in: UserCreate, db: Session = Depends(get_db)):
-    user = create_user(db=db, user_in=user_in)
+@router.post("/client", response_model=UserResponse, summary="Register user client", response_description="Service to register user client in sign in")
+def register_user_client(user_in: UserCreate, db: Session = Depends(get_db)):
+    user = create_user(db=db, user_in=user_in, is_admin=False)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exists",
+        )
+    return user
+
+@router.post("/admin", response_model=UserResponse, summary="Register user admin", response_description="Service to register user admin in sign in")
+def register_user_admin(user_in: UserCreate, db: Session = Depends(get_db)):
+    # breakpoint()
+    user = create_user(db=db, user_in=user_in, is_admin=True)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
